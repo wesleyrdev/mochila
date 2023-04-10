30fa1a8641697d5d35aed871439b8317c3893584
Wesley R. Sales <93395344+wesleyrdev@users.noreply.github.com>
2023-04-10 14:10:57 -0300

    v4

--- js/main.js
+++ js/main.js
@@ -3,5 +3,26 @@ const form = document.getElementById("novoItem")
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-    console.log(evento)
+    criaElemento(evento.target.elements['quantidade'].value)
+    
 })
+
+function criaElemento(nome, quantidade) {
+    console.log(nome)
+    console.log(quantidade)
+
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+
+    novoItem.appendChild = (numeroItem)
+    novoItem.innerHTML += nome
+
+    const lista = document.getElementById("lista")
+
+    lista.appendChild(novoItem)
+
+    console.log(novoItem)
+}
